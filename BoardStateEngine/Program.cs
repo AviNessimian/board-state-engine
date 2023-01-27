3c1108413a053c51a7294a4381bc236a2bd1aa52
avi nessimian <nessimian.avi@outlook.com>
2023-01-27 02:59:47 +0200

    s

--- BoardStateEngine/Program.cs
+++ BoardStateEngine/Program.cs
@@ -1,13 +1,13 @@
 ﻿using BoardStateEngine.Model;
 
-var grid = new int[4, 3]
+var input = new int[4, 3]
 {
     { 0, 1, 0 },
     { 0, 0, 1 },
     { 1, 1, 1 },
     { 0, 0, 0 }
 };
-var board = new Board(grid);
+var board = new Board(input);
 Console.WriteLine("Input:");
 Console.WriteLine(board.ToString());
 
