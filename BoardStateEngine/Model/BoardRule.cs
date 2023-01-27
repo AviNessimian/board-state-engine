c0eef6b6be85213df3ef7482f7e9c41c9f3e30ae
avi nessimian <nessimian.avi@outlook.com>
2023-01-27 02:57:09 +0200

    s

---
+++ BoardStateEngine/Model/BoardRule.cs
@@ -0,0 +1,17 @@
+﻿namespace BoardStateEngine.Model
+{
+    public abstract class BoardRule
+    {
+        public virtual void ValidateLiveState(CellStateTypes state)
+        {
+            if (state != CellStateTypes.Live)
+                throw new Exception($"Rule is for {CellStateTypes.Live} state only");
+        }
+
+        public virtual void ValidateDeadState(CellStateTypes state)
+        {
+            if (state != CellStateTypes.Dead)
+                throw new Exception($"Rule is for {CellStateTypes.Dead} state only");
+        }
+    }
+}
--- BoardStateEngine/Model/IBoardRule.cs
+++ BoardStateEngine/Model/IBoardRule.cs
@@ -4,20 +4,4 @@
     {
         public CellStateTypes Execute(CellStateTypes state, int liveNeighbors);
     }
-
-
-    public abstract class BoardRule
-    {
-        public virtual void ValidateLiveState(CellStateTypes state)
-        {
-            if (state != CellStateTypes.Live)
-                throw new Exception($"Rule is for {CellStateTypes.Live} state only");
-        }
-
-        public virtual void ValidateDeadState(CellStateTypes state)
-        {
-            if (state != CellStateTypes.Dead)
-                throw new Exception($"Rule is for {CellStateTypes.Dead} state only");
-        }
-    }
 }
